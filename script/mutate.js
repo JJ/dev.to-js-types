d2f111b854405a2b15f23f185ac29912809928fc
JJ Merelo <jjmerelo@gmail.com>
2023-07-25 10:55:38 +0200

    Testing different failures

--- script/mutate.js
+++ script/mutate.js
@@ -1,4 +1,10 @@
-import { FloatChromosome } from "../lib/chromosomes.js";
+import { FloatChromosome, StringChromosome } from "../lib/chromosomes.js";
 
 const fChrom = new FloatChromosome([0, 0, 0, 0], 0);
 console.log(FloatChromosome.mutate(fChrom));
+
+const sChrom = new StringChromosome("0001", 1);
+console.log(FloatChromosome.mutate(sChrom));
+
+const notReallyAChrom = { floatVector: [0, 0, 0, 1] };
+console.log(FloatChromosome.mutate(notReallyAChrom));
